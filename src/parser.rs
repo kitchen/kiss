cbd5308261897de715844e6f608abf5d1fe87641
Jeremy Kitchen <kitchen@kitchen.io>
2020-07-12 19:48:30 -0700

    getting more complete

--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,19 @@
-use nom::{map_opt, named, tag, take};
+use nom::{call, delimited, do_parse, map_opt, named, switch, tag, take};
 extern crate num;
 extern crate num_derive;
 use num_derive::FromPrimitive;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 #[allow(dead_code)]
-enum KissFrame {
+pub enum Frame {
     // Data(&[u8]),
-    Command(FrameType),
+    TXDelay(u8),
+    P(u8),
+    SlotTime(u8),
+    TXTail(u8),
+    FullDuplex(bool),
+    // SetHardware(&[u8]),
+    Return,
 }
 
 pub const FEND: u8 = 0xC0;
@@ -33,6 +39,7 @@ named!(pub fend, tag!([FEND]));
 named!(pub tfend, tag!([TFEND]));
 named!(pub fesc, tag!([FESC]));
 named!(pub tfesc, tag!([TFESC]));
+
 named!(
     pub frame_type(&[u8]) -> FrameType,
     map_opt!(
@@ -41,6 +48,68 @@ named!(
             num::FromPrimitive::from_u8(bytes[0])
         }
     )
+);
+
+named!(pub txdelay_frame(&[u8]) -> Frame,
+       do_parse!(
+           txdelay: take!(1) >>
+               (Frame::TXDelay(txdelay[0]))
+       )
+);
+
+named!(pub p_frame(&[u8]) -> Frame,
+       do_parse!(
+           p: take!(1) >>
+               (Frame::P(p[0]))
+       )
+);
+
+named!(pub slot_time_frame(&[u8]) -> Frame,
+       do_parse!(
+           slot_time: take!(1) >>
+               (Frame::SlotTime(slot_time[0]))
+       )
+);
+
+named!(pub txtail_frame(&[u8]) -> Frame,
+       do_parse!(
+           txtail: take!(1) >>
+               (Frame::TXTail(txtail[0]))
+       )
+);
+
+named!(pub fullduplex_frame(&[u8]) -> Frame,
+       do_parse!(
+           fullduplex: take!(1) >>
+               (Frame::FullDuplex(fullduplex[0] != 0))
+       )
+);
+
+named!(pub return_frame(&[u8]) -> Frame,
+       do_parse!(
+           (Frame::Return)
+       )
+);
+
+named!(pub frame_content(&[u8]) -> Frame,
+       switch!(call!(frame_type),
+               // FrameType::Data =>
+               FrameType::TXDelay => call!(txdelay_frame) |
+               FrameType::P => call!(p_frame) |
+               FrameType::SlotTime => call!(slot_time_frame) |
+               FrameType::TXTail => call!(txtail_frame) |
+               FrameType::FullDuplex => call!(fullduplex_frame) |
+               // FrameType::SetHardware =>
+               FrameType::Return => call!(return_frame)
+       )
+);
+
+named!(pub frame(&[u8]) -> Frame,
+       delimited!(
+           fend,
+           frame_content,
+           fend
+       )
 );
 
 #[cfg(test)]
@@ -91,8 +160,68 @@ mod tests {
     }
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_txdelay_frame() {
+        let data = &[42];
+        assert_eq!(Ok((EMPTY, Frame::TXDelay(42))), txdelay_frame(data));
+    }
+
+    #[test]
+    fn test_p_frame() {
+        let data = &[42];
+        assert_eq!(Ok((EMPTY, Frame::P(42))), p_frame(data));
+    }
+
+    #[test]
+    fn test_slot_time_frame() {
+        let data = &[42];
+        assert_eq!(Ok((EMPTY, Frame::SlotTime(42))), slot_time_frame(data));
+    }
+
+    #[test]
+    fn test_txtail_frame() {
+        let data = &[42];
+        assert_eq!(Ok((EMPTY, Frame::TXTail(42))), txtail_frame(data));
+    }
+
+    #[test]
+    fn test_fullduplex_frame() {
+        let data = &[0];
+        assert_eq!(
+            Ok((EMPTY, Frame::FullDuplex(false))),
+            fullduplex_frame(data)
+        );
+
+        let data = &[42];
+        assert_eq!(Ok((EMPTY, Frame::FullDuplex(true))), fullduplex_frame(data));
+    }
+
+    #[test]
+    fn test_return_frame() {
+        assert_eq!(Ok((EMPTY, Frame::Return)), return_frame(EMPTY));
+    }
+
+    #[test]
+    fn test_frame_content() {
+        let data = &[FrameType::TXDelay as u8, 42];
+        assert_eq!(Ok((EMPTY, Frame::TXDelay(42))), frame_content(data));
+
+        let data = &[FrameType::P as u8, 42];
+        assert_eq!(Ok((EMPTY, Frame::P(42))), frame_content(data));
+
+        let data = &[FrameType::SlotTime as u8, 42];
+        assert_eq!(Ok((EMPTY, Frame::SlotTime(42))), frame_content(data));
+
+        let data = &[FrameType::TXTail as u8, 42];
+        assert_eq!(Ok((EMPTY, Frame::TXTail(42))), frame_content(data));
+
+        let data = &[FrameType::FullDuplex as u8, 0];
+        assert_eq!(Ok((EMPTY, Frame::FullDuplex(false))), frame_content(data));
+
+        let data = &[FrameType::FullDuplex as u8, 42];
+        assert_eq!(Ok((EMPTY, Frame::FullDuplex(true))), frame_content(data));
+
+        let data = &[FrameType::Return as u8];
+        assert_eq!(Ok((EMPTY, Frame::Return)), frame_content(data));
     }
 
     #[test]
@@ -119,10 +248,8 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn exit_kiss_mode() {
-        let frame_data: [u8; 3] = [0xC0, 0xFF, 0xC0];
-
-        todo!();
+        let data = &[FEND, FrameType::Return as u8, FEND];
+        assert_eq!(Ok((EMPTY, Frame::Return)), frame(data));
     }
 }
